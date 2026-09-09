5216e3b2db586676ee8e748a4a0c20449215b2a7
NimraNimii <nimiiwritess@gmail.com>
2026-09-09 14:20:09 +0500

    Add X-Frame-Options security header

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
             key: "X-Content-Type-Options",
             value: "nosniff",
           },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
         ],
       },
     ];
